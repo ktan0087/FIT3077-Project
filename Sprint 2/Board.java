5675e14674e042781cd0877f609abe3a2777340a
Kennedy Tan <ktan0087@student.monash.edu>
2023-04-14 09:49:01 +0800

    Create Board class with all the intersections.

---
+++ Sprint 2/Board.java
@@ -0,0 +1,16 @@
+public class Board {
+
+    public static final int MAX_LAYERS = 3;
+    public static final int MAX_POSITIONS = 8;
+    private Intersection[][] intersection = new Intersection[MAX_LAYERS][MAX_POSITIONS];
+    //a varible named currentToken that will take a move in this turn
+
+
+
+
+
+
+    public static void resetBoard(){}
+    //remove all tokens on board
+    //update tokens count
+}

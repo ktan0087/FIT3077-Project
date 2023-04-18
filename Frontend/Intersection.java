260169177d6d188bbff92638efd9ee381998a26d
Soh Meng Jienq <msoh0007@student.monash.edu>
2023-04-19 01:03:36 +0800

    Align intersection and blank label, make it in 3x3 grid

---
+++ Frontend/Intersection.java
@@ -0,0 +1,164 @@
+import javax.swing.*;
+import java.awt.*;
+
+// Customized all intersections
+public class Intersection extends JLabel {
+    // the enum is used to determine the position of intersection
+    public enum Position{
+        TOP_LEFT,
+        TOP_MIDDLE,
+        TOP_RIGHT,
+        MIDDLE_LEFT,
+        MIDDLE,
+        MIDDLE_RIGHT,
+        BOTTOM_LEFT,
+        BOTTOM_MIDDLE,
+        BOTTOM_RIGHT
+    }
+    protected Boolean isWhite = false;
+    CircleButton inter = new CircleButton(); // create a circle button for intersection
+    public Intersection(){
+        this.setLayout(new GridLayout(3, 3, -1, -1)); // -1 is to prevent showing grid border
+    }
+
+    // This method is used to choose the type of intersection by selecting the position of it
+    public void choosePosition(Position position){
+        switch (position){
+            case TOP_LEFT:
+                this.add(new JLabel());
+                this.add(new JLabel());
+                this.add(new JLabel());
+
+                this.add(new JLabel());
+                this.add(inter);
+                this.add(new HorizontalLine());
+
+                this.add(new JLabel());
+                this.add(new VerticalLine());
+                this.add(new JLabel());
+
+                break;
+
+            case TOP_MIDDLE:
+                this.add(new JLabel());
+                this.add(new JLabel());
+                this.add(new JLabel());
+
+                this.add(new HorizontalLine());
+                this.add(inter);
+                this.add(new HorizontalLine());
+
+                this.add(new JLabel());
+                this.add(new VerticalLine());
+                this.add(new JLabel());
+
+                break;
+
+            case TOP_RIGHT:
+                this.add(new JLabel());
+                this.add(new JLabel());
+                this.add(new JLabel());
+
+                this.add(new HorizontalLine());
+                this.add(inter);
+                this.add(new JLabel());
+
+                this.add(new JLabel());
+                this.add(new VerticalLine());
+                this.add(new JLabel());
+
+                break;
+
+            case MIDDLE_LEFT:
+                this.add(new JLabel());
+                this.add(new VerticalLine());
+                this.add(new JLabel());
+
+                this.add(new JLabel());
+                this.add(inter);
+                this.add(new HorizontalLine());
+
+                this.add(new JLabel());
+                this.add(new VerticalLine());
+                this.add(new JLabel());
+
+                break;
+
+            case MIDDLE:
+                this.add(new JLabel());
+                this.add(new VerticalLine());
+                this.add(new JLabel());
+
+                this.add(new HorizontalLine());
+                this.add(inter);
+                this.add(new HorizontalLine());
+
+                this.add(new JLabel());
+                this.add(new VerticalLine());
+                this.add(new JLabel());
+
+                break;
+
+            case MIDDLE_RIGHT:
+                this.add(new JLabel());
+                this.add(new VerticalLine());
+                this.add(new JLabel());
+
+                this.add(new HorizontalLine());
+                this.add(inter);
+                this.add(new JLabel());
+
+                this.add(new JLabel());
+                this.add(new VerticalLine());
+                this.add(new JLabel());
+
+                break;
+
+            case BOTTOM_LEFT:
+                this.add(new JLabel());
+                this.add(new VerticalLine());
+                this.add(new JLabel());
+
+                this.add(new JLabel());
+                this.add(inter);
+                this.add(new HorizontalLine());
+
+                this.add(new JLabel());
+                this.add(new JLabel());
+                this.add(new JLabel());
+
+                break;
+
+            case BOTTOM_MIDDLE:
+                this.add(new JLabel());
+                this.add(new VerticalLine());
+                this.add(new JLabel());
+
+                this.add(new HorizontalLine());
+                this.add(inter);
+                this.add(new HorizontalLine());
+
+                this.add(new JLabel());
+                this.add(new JLabel());
+                this.add(new JLabel());
+
+                break;
+
+            case BOTTOM_RIGHT:
+                this.add(new JLabel());
+                this.add(new VerticalLine());
+                this.add(new JLabel());
+
+                this.add(new HorizontalLine());
+                this.add(inter);
+                this.add(new JLabel());
+
+                this.add(new JLabel());
+                this.add(new JLabel());
+                this.add(new JLabel());
+
+                break;
+        }
+    }
+
+}

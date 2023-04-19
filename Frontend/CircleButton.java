ee23bdd7ae21d98cbbd1f436697bb8868a3a24fc
Soh Meng Jienq <msoh0007@student.monash.edu>
2023-04-20 01:28:09 +0800

    Delete useless function

--- Frontend/CircleButton.java
+++ Frontend/CircleButton.java
@@ -1,8 +1,5 @@
-// REFERENCE: https://www.roseindia.net/tutorial/java/swing/createRoundButton.html
-
 import javax.swing.*;
 import java.awt.*;
-import java.awt.geom.Ellipse2D;
 
 // Customized circle button
 public class CircleButton extends JButton {
@@ -10,23 +7,13 @@ public class CircleButton extends JButton {
 
     public CircleButton(){
 //        super(label);
-        this.setBackground(new Color(0x000000));
 //        this.label = label;
-        Dimension size = getPreferredSize();
-        size.width = size.height = Math.max(size.width,size.height);
-        setPreferredSize(size);
-
+        this.setBackground(new Color(0x000000));
         setContentAreaFilled(false);
     }
 
     protected void paintComponent(Graphics g) {
-        if (getModel().isArmed()) {
-            g.setColor(Color.lightGray);
-        } else {
-            g.setColor(getBackground());
-        }
         g.fillOval(0, 0, getSize().width-1,getSize().height-1);
-
         super.paintComponent(g);
     }
 
@@ -35,12 +22,4 @@ public class CircleButton extends JButton {
         g.drawOval(0, 0, getSize().width-1,getSize().height-1);
     }
 
-    Shape shape;
-    public boolean contains(int x, int y) {
-        if (shape == null ||
-                !shape.getBounds().equals(getBounds())) {
-            shape = new Ellipse2D.Float(0, 0, getWidth(), getHeight());
-        }
-        return shape.contains(x, y);
-    }
 }

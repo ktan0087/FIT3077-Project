42a7de2b7d47f6ce91de99b36496566012ea1675
Soh Meng Jienq <msoh0007@student.monash.edu>
2023-04-20 01:12:58 +0800

    Update comments for PlaceToken class

--- Frontend/PlaceToken.java
+++ Frontend/PlaceToken.java
@@ -1,12 +1,13 @@
 import javax.swing.*;
 import java.awt.*;
 
+// PlaceToken is used to place the token on the board
 public class PlaceToken extends JPanel {
     public PlaceToken(){
         this.setOpaque(false); // set the background of this panel transparent
         this.setPreferredSize(new Dimension(500,500)); // set the size of the board
 
-        // set the layout of the board, 13 rows and 13 columns
+        // set the layout, the layout must be the same as board (13 rows and 13 columns)
         this.setLayout(new GridLayout(13, 13, -1, -1)); // -1 is to remove the space between intersections and lines
 
         // create placeholder

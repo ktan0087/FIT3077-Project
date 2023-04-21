5ae9d0c1cfcab345ddeab8edf553d65c754fe542
Soh Meng Jienq <msoh0007@student.monash.edu>
2023-04-21 21:07:08 +0800

    Finalise frontend buttons

---
+++ Frontend/BtnPlay.java
@@ -0,0 +1,18 @@
+package Frontend;
+
+import javax.swing.JButton;
+import javax.swing.BorderFactory;
+import java.awt.Font;
+import java.awt.Color;
+
+// Customized button for PLAY that starts the game
+public class BtnPlay extends JButton {
+    public BtnPlay(){
+        this.setText("PLAY"); // set the text of button
+        this.setFocusable(false); // get rid of the annoying box in button
+        this.setFont(new Font("Inter",Font.BOLD, 80));
+        this.setForeground(new Color(0x000000));
+        this.setBackground(new Color(0xE59C54));
+        this.setBorder(BorderFactory.createMatteBorder(10, 10, 10, 10, Color.BLACK));
+    }
+}

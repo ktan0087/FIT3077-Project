8f802f079d21d1824bb7a320d567e7bc36b2c748
Soh Meng Jienq <msoh0007@student.monash.edu>
2023-05-17 19:58:38 +0800

    Create instructions to guide users during gameplay; correct the comment in Application

--- Frontend/InitialBoard.java
+++ Frontend/InitialBoard.java
@@ -21,6 +21,7 @@ public class InitialBoard extends JPanel {
     private BlackTokenRemain blackTokenRemain = new BlackTokenRemain(); // show the remaining number of black tokens
     private Token selectedToken; // the token that is selected by the player
     protected boolean isSelected; // whether the player has selected a token
+    private Instruction instruction = new Instruction(Instruction.InstructionType.EMPTY); // provide the instruction of the game
     private Game game; // the game that is played
     private PlaceToken millLayer = new PlaceToken(); // the layer that shows the mill
     protected ResultButton resultButton = new ResultButton();
@@ -62,6 +63,15 @@ public class InitialBoard extends JPanel {
         gbc.weighty = 0; // vertical spacing
         gbc.anchor = GridBagConstraints.NORTHEAST; // set the position of the component
         this.add(buttons, gbc); // add the component to this panel
+
+        // add the board to the center of the panel
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        gbc.weightx = 1;
+        gbc.weighty = 0;
+        gbc.anchor = GridBagConstraints.NORTH;
+        gbc.gridwidth = 3;
+        this.add(instruction, gbc);
 
         // add the board to the center of the panel
         gbc.gridx = 1;
@@ -199,6 +209,7 @@ public class InitialBoard extends JPanel {
                     if (!checkSelected()) { // if no white token is selected
                         PlaceTokenAction newPlaceAction = new PlaceTokenAction(getGame().getCurrentPlayer(),getGame().getBoard().getIntersection(intersection.getCoordinateX(),intersection.getCoordinateY()), getGame());
                         if(newPlaceAction.execute()){
+                            instruction.changeText(Instruction.InstructionType.PLACE);
                             placeToken.remove(index); // remove the placeholder at the same index
                             placeToken.add(token, index); // add white token at the same index
                             placeToken.repaint();
@@ -213,6 +224,7 @@ public class InitialBoard extends JPanel {
                     // Check if a mill is formed
                     if (game.getBoard().getMills().size() > 0){
                         if (millCount != game.getBoard().getMills().size()){
+                            instruction.changeText(Instruction.InstructionType.REMOVE);
                             game.swapPlayers();
                             playerTurn.changeIcon();
                             millCount = game.getBoard().getMills().size();
@@ -275,7 +287,10 @@ public class InitialBoard extends JPanel {
             whiteTokenRemain.decreaseAmountToken(); // decrease the white token remainder
         }
         else {
-            blackTokenRemain.decreaseAmountToken(); // decrease the black token remainder
+            int blackRemaining = blackTokenRemain.decreaseAmountToken(); // decrease the black token remainder
+            if (blackRemaining == 0){
+                instruction.changeText(Instruction.InstructionType.MOVE);
+            }
         }
     }
 
--- Frontend/TokenRemain.java
+++ Frontend/TokenRemain.java
@@ -8,11 +8,12 @@ public abstract class TokenRemain extends JLabel {
      * This class is used to show the remaining token
      */
     private ImageIcon tokenIcon;
-    private int amountToken = 9; // default amount of token
+    private int amountToken; // default amount of token
 
     // Constructor
     public TokenRemain(){
-        this.setText("9");
+        this.amountToken = 9;
+        this.setText(String.valueOf(amountToken));
         this.setBackground(new Color(0xE6B380));
         this.setFont(new Font("Inter", Font.PLAIN, 42));
         this.setForeground(new Color(0x000000));
@@ -29,10 +30,11 @@ public abstract class TokenRemain extends JLabel {
     /**
      * This method is used to decrease the amount of token remainder
      */
-    public void decreaseAmountToken() {
+    public int decreaseAmountToken() {
         if (this.amountToken > 0){
             this.amountToken--;
             this.setText(Integer.valueOf(amountToken).toString());
         }
+        return this.amountToken;
     }
 }

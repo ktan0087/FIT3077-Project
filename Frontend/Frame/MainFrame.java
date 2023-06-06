cfe6be94cecaefae18d68ea5b70f58ce5452c1f5
Kennedy Tan <ktan0087@student.monash.edu>
2023-06-06 18:05:42 +0800

    change window location to open on the center of screen

--- Frontend/Frame/MainFrame.java
+++ Frontend/Frame/MainFrame.java
@@ -15,6 +15,7 @@ public class MainFrame extends JFrame {
     public MainFrame(){
         this.setTitle("NINE MEN'S MORRIS"); // set title of frame
         this.setSize(new Dimension(1280, 720)); // set the x-dimension and y-dimension of frame
+        this.setLocationRelativeTo(null);
         this.setVisible(true); // make the frame visible
         this.setResizable(false); // prevent frame from being resized
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); // exit out of application
